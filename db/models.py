dd01eac0c0cb6cb236420580378556d617115e1b
hamidisaac <isaac.hamid@gmail.com>
2023-09-04 14:08:16 +0330

    add Post model and schema

--- db/models.py
+++ db/models.py
@@ -1,5 +1,6 @@
 from db.database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = 'users'
@@ -7,3 +8,15 @@ class User(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+    posts = relationship('Post', back_populates='user')
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, index=True, primary_key=True)
+    image_url = Column(String)
+    image_url_type = Column(String)
+    caption = Column(String)
+    timestamp = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='posts')
+
--- db/schemas.py
+++ db/schemas.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel
+from datetime import datetime
+
+class User(BaseModel):
+    username: str
 
 class UserBase(BaseModel):
     username: str
@@ -8,3 +12,17 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str
     email: str
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    creator_id: int
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
